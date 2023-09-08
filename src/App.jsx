6fd5e5b8b750d5d95f418d3bbb7ffacf6d7dc09e
Sahil kumar <sahilkumarofficial05@gmail.com>
2023-09-08 16:38:26 +0530

    project started

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <div className=" relative pt-12 bg-[url(https://cdn.pixabay.com/photo/2016/11/21/18/14/wall-1846965_1280.jpg)] h-screen w-screen bg-cover bg-center bg-fixed">
+      <h1 className="absoulute mx-auto py-2  w-10/12 bg-white text-4xl font-bold rounded-lg text-center">Random GIF generator</h1>
+    </div>
+  )
+}
+
+export default App
